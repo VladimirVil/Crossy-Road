531d7e5712f8ebf55931e807758ee8e4d2d234c8
Vova <vilenchik.vladimir@gmail.com>
2019-03-22 10:47:04 +0100

    Fixed camera follow

--- Assets/Crossy Road/Skripts/CameraFollow.cs
+++ Assets/Crossy Road/Skripts/CameraFollow.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,7 +10,9 @@ public class CameraFollow : MonoBehaviour {
     public GameObject player = null;
     //camera location
     //public Vector3 offset = new Vector3(3, 6, -3);
-    public Vector3 offset = new Vector3(0, 6, 0);
+    public Vector3 offset = new Vector3(-0.5f, 4, -0.8f);  // offset between player and camera object
+   // public Vector3 offset = new Vector3(26, 26, 0);
+
     Vector3 depth = Vector3.zero;   //how high the camera is 
     Vector3 pos = Vector3.zero;
 
@@ -21,20 +23,50 @@ public class CameraFollow : MonoBehaviour {
 
         if (autoMove)
         {
+            //depth - how high is the camera located    
             //camera should go forward with the player 
             depth = this.gameObject.transform.position += new Vector3(0, 0, speed * Time.deltaTime);  //depth is speed going forward
-            //depth = this.gameObject.transform.position += new Vector3((speed/2)*Time.deltaTime, 0, (speed / 2) * Time.deltaTime);  //depth is speed going forward
+           // depth = this.gameObject.transform.position += new Vector3((speed/2)*Time.deltaTime, 0, (speed / 2) * Time.deltaTime);  //depth is speed going forward
 
             pos = Vector3.Lerp(gameObject.transform.position, player.transform.position + offset, Time.deltaTime);   //capturing the position between the camera 
                                                                                                                      //and the player  then passed to the actual camera position 
                                                                                                                      //gameObject.transform.position = new Vector3(pos.x, offset.y, depth.z);  // Offset - how high above we wanna be 
-         gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z);  // Offset - how high above we wanna be 
-         //gameObject.transform.position = new Vector3(0, pos.y, 0); 
+
+           // gameObject.transform.position = new Vector3(pos.x, offset.y, depth.z);
+        //gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z);  // Offset - how high above we wanna be 
+         gameObject.transform.position = new Vector3(0, pos.y, 0); 
         }
         else
         {
             pos = Vector3.Lerp(gameObject.transform.position, player.transform.position + offset, Time.deltaTime);
             gameObject.transform.position = new Vector3(pos.x, offset.y, pos.z);
         }
+    }
+}
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+
+    private Vector3 offset = new Vector3(0, 4, -2);         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+       // offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;
     }
 }
